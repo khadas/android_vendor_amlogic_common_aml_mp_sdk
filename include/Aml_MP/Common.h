8bab36493d0e95f01cf430866fba113b6c71dbef
jianchuan.ping <jianchuan.ping@amlogic.com>
2021-07-06 17:06:05 +0800

    Aml_mp: Set audio Presentation ID. [1/2]
    PD#SWPL-52872
    
    Problem:
    MS12 need add support for set Presentation ID
    
    Solution:
    Support set Presentation ID
    
    Verify:
    AH212
    
    Change-Id: I8c77af8f19b64df9a2411f069d2c4c27cce5e045
    Signed-off-by: jianchuan.ping <jianchuan.ping@amlogic.com>

--- include/Aml_MP/Common.h
+++ include/Aml_MP/Common.h
@@ -270,6 +270,7 @@ typedef enum {
     AML_MP_PLAYER_PARAMETER_VENDOR_ID,                      //setVendorID(int*)
     AML_MP_PLAYER_PARAMETER_VIDEO_TUNNEL_ID,                //setVideoTunnelID(int*)
     AML_MP_PLAYER_PARAMETER_SURFACE_HANDLE,                 //setSurface(void*)
+    AML_MP_PLAYER_PARAMETER_AUDIO_PRESENTATION_ID,          //setPresentationId(int*)
 
     //get only
     AML_MP_PLAYER_PARAMETER_GET_BASE        = 0x2000,
